022c2a57d623e85eb19c13fd3bb567c2df6a36a7
ikhsanfirdauz0 <ikhsan.firdauz@ui.ac.id>
2021-12-02 18:12:35 +0700

    first commit

---
+++ src/auth/auth.controller.ts
@@ -0,0 +1,17 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { UserService } from 'src/user/user.service';
+
+@Controller()
+export class AuthController {
+
+    constructor(private userService: UserService)
+    {
+
+    }
+
+    @Post('register')
+    async register(@Body() body)
+    {
+        return body;
+    }
+}
